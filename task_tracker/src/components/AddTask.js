2fd03df154858c242a8ae558af968a19319fd939
dimuthi tharaka <dimuthi@gmail.com>
2022-02-17 13:31:27 +0530

    form states

--- task_tracker/src/components/AddTask.js
+++ task_tracker/src/components/AddTask.js
@@ -1,24 +1,30 @@
+import {useState} from 'react';
+
 const AddTask = () => {
+     const[task, setTask] = useState('');
+ const[day, setDay] = useState('');
+ const[reminder, setReminder] = useState(false);
+
     return(
         <form className="add-form">
         <div className="form-control">
             <label>
                 Task
             </label>
-            <input type="text" placeholder="Enter Task Name"/>
+            <input type="text" placeholder="Enter Task Name" value={task} onChange={(e)=> setTask(e.target.value)}/>
          
             </div>
              <div className="form-control">
             <label>
                Date & Time
             </label>
-            <input type="text" placeholder="Enter Date and Time"/>
+            <input type="text" placeholder="Enter Date and Time" value={day} onChange={(e)=> setDay(e.target.value)}/>
              </div>
              <div className="form-control" className="form-control-check">
             <label>
                Reminder
             </label>
-            <input type="checkbox" />
+            <input type="checkbox" value={reminder} onChange={(e)=> setDay(e.currentTarget.value)}/>
             </div>
             <div>
 <input type="submit" value="save task" className="btn btn-block" />
